5604143a84f02aa0157d62f22ccc44aefad63954
pietoro29 <icepietoro@gmail.com>
2026-01-08 22:39:34 +0900

    Completion of step0

--- net.c
+++ net.c
@@ -5,17 +5,34 @@
 int
 net_init(void)
 {
+    infof( "initialize...");
+    if (platform_init() == -1) {
+        errorf( "platform_init() failure");
+        return -1;
+    }
+    infof( "success");
     return 0;
 }
 
 int
 net_run(void)
 {
+    infof( "startup...");
+    if (platform_run() == -1) {
+        errorf( "platform_run() failure");
+        return -1;
+    }
+    infof( "success");
     return 0;
 }
 
 int
 net_shutdown(void)
 {
+    infof( "shutting down...");
+    if (platform_shutdown() == -1) {
+        warnf( "platform_shutdown() failure");
+    }
+    infof( "success");
     return 0;
 }
