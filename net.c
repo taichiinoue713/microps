efe3dc4454c4f3d03ce9b75b8c96ec0dfd028a19
pietoro29 <icepietoro@gmail.com>
2026-01-10 17:30:16 +0900

    Completion of step3

--- net.c
+++ net.c
@@ -6,6 +6,7 @@
 
 #include "util.h"
 #include "net.h"
+#include "ip.h"
 
 struct net_protocol {
     struct net_protocol *next;
@@ -115,13 +116,41 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
 int
 net_protocol_register(uint16_t type, net_protocol_handler_t handler)
 {
+    struct net_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            errorf("already registered, type=0x%04x", proto->type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if (!proto) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("success, type=0x%04x", type);
+    return 0;
 }
 
 int
 net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    struct net_protocol *proto;
+
     debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
+    for (proto =protocols; proto; proto = proto->next) {
+        if (proto->type == type) {
+            proto->handler(data, len, dev);
+            return 0;
+        }
+    }
+    /* unsupported protocol*/
     return 0;
 }
 
@@ -131,6 +160,10 @@ net_init(void)
     infof( "initialize...");
     if (platform_init() == -1) {
         errorf( "platform_init() failure");
+        return -1;
+    }
+    if (ip_init() == -1) {
+        errorf("ip_init() failure");
         return -1;
     }
     infof( "success");
